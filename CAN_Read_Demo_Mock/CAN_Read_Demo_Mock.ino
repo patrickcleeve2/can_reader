f097fdbc062a4382b5a5269b30940bd9a32eae84
Patrick Cleeve <patrickcleeve2@gmail.com>
2021-08-21 19:11:30 +1000

    add multiple random can ids to publish

--- CAN_Read_Demo_Mock/CAN_Read_Demo_Mock.ino
+++ CAN_Read_Demo_Mock/CAN_Read_Demo_Mock.ino
@@ -1,16 +1,26 @@
+
+String valid_can_ids[4] = {"0x690", "0x545", "0x80", "5e4"};
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); 
   Serial.println("Sending Mock CAN Frame Data");
+
 
 }
 
 void loop() {
 
   // Create a fake CAN Frame for testing.
- 
+  Serial.print("can,");
+
   // Create ID in hex (TODO: randomise ID too)
-  Serial.print("can,0x690,");
+
+  int idx = sizeof(valid_can_ids) / sizeof(String);
+  String frame_id = valid_can_ids[random(idx)];
+  
+  Serial.print(frame_id);
+  Serial.print(",");
 
   // Generate random data bytes in hex
   for (int i = 0; i <= 7; i++) {
