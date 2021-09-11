c5b97112c3d36ba52081edd099200f8fb11c7ec9
Patrick Cleeve <patrickcleeve2@gmail.com>
2021-09-11 19:01:10 +1000

    added vehicle configs, cleaned up names

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # can_reader
-can reader demo
+
+An openpilot style CAN interface using opendbc, arduino, and ROS.
+
+This package reads CAN frames sent over serial via an Arduino, and republishes the decoded frames in ROS.
+- It can also be used directly with a CAN-USB device and [socketcan](http://wiki.ros.org/socketcan_interface) to publish decoded CAN frames into ROS.
+
+## Setup
+To permanently enable read to port /dev/ttyACM0
+``` bash
+$ sudo usermod -a -G dialout $USER
+$ sudo reboot 
+```
+
+To enable for one time:
+``` bash
+ $ sudo chmod 666 /dev/ttyACM0 
+```
+
+## Getting Started
+- Clone this repository into your catkin workspace.
+- Plug in the Arduino / USB CAN Reader (default port is /dev/ttyACM0).
+- Select your vehicle config (or write your own).
+
+
+```
+$ catkin_make
+$ source devel/setup.bash
+```
+ and run
+
+``` bash
+$ roslaunch can_decoder can_decoder.launch
+```
+
+Topics Published:
+- can_msgs/Frame: ```/mini/can``` 
+- can_decoder/CarState:  ```/mini/vehicle_state```
+
+## Known Issues:
+- The mock arduino publisher sometimes causes bad frames to be sent, which will not be decoded, and sometimes crash. Just restart it.
+- Hasn't been used to send CAN data back to the car.
--- can_decoder/src/can_publisher.py
+++ can_decoder/src/can_publisher.py
@@ -6,15 +6,17 @@ from pprint import pprint
 import cantools
 import rospy
 import serial
-from can_msgs.msg import Frame
+import yaml
 from can_decoder.msg import CarState
+from can_msgs.msg import Frame
 
-KPH_TO_MS = 1. / 3.6
+KPH_TO_MS = 1.0 / 3.6
 # ref
 # https://github.com/commaai/openpilot/blob/master/selfdrive/car/hyundai/carstate.py
 
+
 class CANDecoder(object):
-    def __init__(self, dbc_file) -> None:
+    def __init__(self, dbc_file, config) -> None:
         super().__init__()
 
         self.db = cantools.database.load_file(
@@ -22,52 +24,73 @@ class CANDecoder(object):
             strict=False,
         )
 
+        self.config = self.load_config(config)
+
         self.can_data = {}
-        self.total_msgs = 162
+        self.total_msgs = self.config["total_messages"]
 
         self.can_sub = rospy.Subscriber("/mini/can", Frame, self.can_callback)
         self.state_pub = rospy.Publisher("mini/vehicle_state", CarState, queue_size=1)
+
+    def load_config(self, config):
+
+        with open(config, "r") as f:
+
+            config_dict = yaml.safe_load(f)
+            rospy.loginfo(f"CONFIG LOADED: {config}")
+
+        return config_dict
 
     def can_callback(self, msg):
 
         message_id = msg.id
         message_data = list(msg.data)
         # rospy.loginfo(f"{msg.id}: {list(msg.data)}")
-        
+
         message = self.db.decode_message(message_id, message_data)
         self.can_data.update(message)
         # pprint(self.can_data)
-        
-        if len(self.can_data) == self.total_msgs: # only publish once all msgs are received
-            rospy.loginfo_once(f"starting to publish: {len(self.can_data)}/{self.total_msgs}")
+
+        if (
+            len(self.can_data) == self.total_msgs
+        ):  # only publish once all msgs are received
+            rospy.loginfo_once(
+                f"starting to publish: {len(self.can_data)}/{self.total_msgs}"
+            )
             self.publish_state()
         else:
-            rospy.loginfo_throttle(0.25, f"waiting on msgs: {len(self.can_data)}/{self.total_msgs}")
+            rospy.loginfo_throttle(
+                0.25, f"waiting on msgs: {len(self.can_data)}/{self.total_msgs}"
+            )
 
     def publish_state(self):
 
         # get car velocity from wheel speeds
-        wheel_speed_fl = self.can_data["WHL_SPD_FL"] *KPH_TO_MS
-        wheel_speed_fr = self.can_data["WHL_SPD_FR"] *KPH_TO_MS
-        wheel_speed_ll = self.can_data["WHL_SPD_RL"] *KPH_TO_MS
-        wheel_speed_rr = self.can_data["WHL_SPD_RR"] *KPH_TO_MS
-        v_ego = (wheel_speed_fl + wheel_speed_fr + wheel_speed_ll + wheel_speed_rr) / 4.0
-    
+        wheel_speed_fl = self.can_data[self.config["wheel_speed"]["FL"]] * KPH_TO_MS
+        wheel_speed_fr = self.can_data[self.config["wheel_speed"]["FR"]] * KPH_TO_MS
+        wheel_speed_ll = self.can_data[self.config["wheel_speed"]["RL"]] * KPH_TO_MS
+        wheel_speed_rr = self.can_data[self.config["wheel_speed"]["RR"]] * KPH_TO_MS
+        v_ego = (
+            wheel_speed_fl + wheel_speed_fr + wheel_speed_ll + wheel_speed_rr
+        ) / 4.0
 
         msg = CarState()
         msg.header.stamp = rospy.Time.now()
         msg.header.frame_id = "car"
-        msg.throttle_position = self.can_data["Accel_Pedal_Pos"] # / 254.
+        msg.throttle = self.can_data[self.config["control"]["throttle"]]  # / 254.
         msg.velocity = v_ego
-        msg.steering_angle = self.can_data["SAS_Angle"]
-        msg.steering_rate = self.can_data["SAS_Speed"]
-        msg.brake_pressed = self.can_data["DriverBraking"]  # nb this is not brake position, just a flag
+        msg.steer = self.can_data[self.config["control"]["steer"]]
+        msg.steer_rate = self.can_data[self.config["control"]["steer_rate"]]
+        msg.brake_pressed = self.can_data[
+            self.config["control"]["brake_pressed"]
+        ]  # nb this is not brake position, just a flag
 
-        msg.yaw_rate = self.can_data["YAW_RATE"]
-        msg.left_blinker = self.can_data["CF_Gway_TurnSigLh"]
-        msg.right_blinker = self.can_data["CF_Gway_TurnSigRh"]
-        
+        msg.yaw_rate = self.can_data[self.config["control"]["yaw_rate"]]
+        msg.left_indicator = self.can_data[self.config["indicator"]["left"]]
+        msg.right_indicator = self.can_data[self.config["indicator"]["right"]]
+
         self.state_pub.publish(msg)
+
 
 # TODO: door open, seatbelt, acc, lka, steer torque
 
@@ -76,10 +99,14 @@ if __name__ == "__main__":
     rospy.init_node("can_decoder")
     rospy.loginfo("hello can decoder")
 
+    # TODO: move these to a relative path based on $(find...)
+    # https://answers.ros.org/question/235337/unable-to-read-a-file-while-using-relative-path/
+
     # dbc_file = "/home/patrick/self_drive/repos/can_demo/opendbc/hyundai_i30_2014.dbc"
     dbc_file = "/home/patrick/self_drive/repos/can_demo/opendbc/hyundai_kia_generic.dbc"
-    can_decoder = CANDecoder(dbc_file=dbc_file)
-    
+    config_file = "/home/patrick/self_drive/catkin_ws/src/can_reader/can_decoder/config/hyundai_config.yaml"
+    can_decoder = CANDecoder(dbc_file=dbc_file, config=config_file)
+
     rospy.spin()
 
 
